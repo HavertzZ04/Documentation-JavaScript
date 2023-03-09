35869c1c3f20a6ce0d826df6be991bbd43034224
HavertzZ04 <havertzz04@gmail.com>
2023-03-08 21:09:34 -0500

    feat: :rocket: Math.exp()
    The Math.exp() static method returns e raised to the power of a number. That is 𝙼𝚊𝚝𝚑.𝚎𝚡𝚙( 𝚡 ) = e**2

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-function difference(a, b) {
-    return Math.abs(a - b);
-  }
-  
-  console.log(difference(3, 5));
-  // Expected output: 2
-  
-  console.log(difference(5, 3));
-  // Expected output: 2
-  
-  console.log(difference(1.23456, 7.89012));
-  // Expected output: 6.6555599999999995
+console.log(Math.exp(0));
+// Expected output: 1
+
+console.log(Math.exp(1));
+// Expected output: 2.718281828459 (approximately)
+
+console.log(Math.exp(-1));
+// Expected output: 0.36787944117144233
+
+console.log(Math.exp(2));
+// Expected output: 7.3890560989306
