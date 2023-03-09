031753a8c91a42544f4e2d0a4bcae14c5400c871
HavertzZ04 <havertzz04@gmail.com>
2023-03-08 20:21:11 -0500

    feat: :rocket: Inequable
    Returns true if the operands are not equal

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ let numberTwo = "15";
 let numberThree = 20;
 let numberFour = 15;
 
-console.log(numberOne == numberThree); // Returns False
-console.log(numberOne == numberTwo); // Returns True
-console.log(numberOne == numberFour); // Returns true
+console.log(numberOne != numberThree); // Returns True
+console.log(numberOne != numberFour); // Returns False
