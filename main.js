616d1e6701823c90f523e88e3505d759454a0cc4
HavertzZ04 <havertzz04@gmail.com>
2023-03-08 20:20:21 -0500

    feat: :rocket: Equal
    Returns true if the operands are equal and false if they are different

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-let age = 24;
-console.log(age); //Returns 24
+let numberOne = 15;
+let numberTwo = "15";
+let numberThree = 20;
+let numberFour = 15;
+
+console.log(numberOne == numberThree); // Returns False
+console.log(numberOne == numberTwo); // Returns True
+console.log(numberOne == numberFour); // Returns true
