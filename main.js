b227047d7633167b2e89408bb366e8fc5e220692
HavertzZ04 <havertzz04@gmail.com>
2023-03-08 21:17:05 -0500

    feat: :rocket: Math.pow()
    The Math.pow() static method returns the value of a base raised to a power

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-console.log(Math.min(2, 3, 1));
-// Expected output: 1
+console.log(Math.pow(7, 3));
+// Expected output: 343
 
-console.log(Math.min(-2, -3, -1));
-// Expected output: -3
+console.log(Math.pow(4, 0.5));
+// Expected output: 2
 
-const array1 = [2, 3, 1];
+console.log(Math.pow(7, -2));
+// Expected output: 0.02040816326530612
+//                  (1/49)
 
-console.log(Math.min(...array1));
-// Expected output: 1
+console.log(Math.pow(-7, 0.5));
+// Expected output: NaN
+
   
