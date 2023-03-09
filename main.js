60fc875ae47889c86b321fa39c9ff0318306bac2
HavertzZ04 <havertzz04@gmail.com>
2023-03-08 21:34:56 -0500

    feat: :rocket: Math.ceil()
    The Math.ceil() static method always rounds up and returns the smaller integer greater than or equal to a given number.

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-console.log(Math.floor(5.95));
-// Expected output: 5
+console.log(Math.ceil(.95));
+// Expected output: 1
 
-console.log(Math.floor(5.05));
-// Expected output: 5
+console.log(Math.ceil(4));
+// Expected output: 4
 
-console.log(Math.floor(5));
-// Expected output: 5
+console.log(Math.ceil(7.004));
+// Expected output: 8
 
-console.log(Math.floor(-5.05));
-// Expected output: -6
-  
+console.log(Math.ceil(-7.004));
+// Expected output: -7
