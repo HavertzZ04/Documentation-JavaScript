b63e1f94f731d220f517c4f1bf8a72d41511653d
HavertzZ04 <havertzz04@gmail.com>
2023-03-08 21:34:00 -0500

    feat: :rocket: Math.floor()
    The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-console.log(Math.round(0.9));
-// Expected output: 1
+console.log(Math.floor(5.95));
+// Expected output: 5
 
-console.log(Math.round(5.95), Math.round(5.5), Math.round(5.05));
-// Expected output: 6 6 5
+console.log(Math.floor(5.05));
+// Expected output: 5
 
-console.log(Math.round(-5.05), Math.round(-5.5), Math.round(-5.95));
-// Expected output: -5 -5 -6
+console.log(Math.floor(5));
+// Expected output: 5
+
+console.log(Math.floor(-5.05));
+// Expected output: -6
   
