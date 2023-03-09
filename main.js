15d8ccf0ffea722f79996e38daa80bffd4fe3b41
HavertzZ04 <havertzz04@gmail.com>
2023-03-08 21:16:02 -0500

    feat: :rocket: Math.min()
    The Math.min() static method returns the smallest of the numbers given as input parameters, or Infinity if there are no parameters.

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-console.log(Math.max(1, 3, 2));
-// Expected output: 3
+console.log(Math.min(2, 3, 1));
+// Expected output: 1
 
-console.log(Math.max(-1, -3, -2));
-// Expected output: -1
+console.log(Math.min(-2, -3, -1));
+// Expected output: -3
 
-const array1 = [1, 3, 2];
+const array1 = [2, 3, 1];
 
-console.log(Math.max(...array1));
-// Expected output: 3
-
+console.log(Math.min(...array1));
+// Expected output: 1
   
