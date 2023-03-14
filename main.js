8b78dbcc5ef2e484752fa581f84d762e0002a9fc
HavertzZ04 <havertzz04@gmail.com>
2023-03-14 13:11:37 -0500

    feat: :rocket: Lexical scope of this
    one of the main differences between arrow functions and traditional functions is the value of the this keyword, which is not always the same

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let a = function () {
-    return "Tradicional one";
-};
+father = {
+    a: function () {
+      console.log(this);
+    },
+    b: () => {
+      console.log(this);
+    },
+  };
   
-let b = () => {
-    return "Arrow one.";
-};
-
-const c = () => "Arrow one"; // 0 parameter: return "Arrow one"
-const d = (e) => e + 1; // 1 parameter: Return the value of  e + 1
-const e = (a, b) => a + b; // 2 parameter: Return the value of a + b
+  father.a(); // father
+  father.b(); // Window
