391317faf6ff5783fe03d4cc091c0ac136811bc4
HavertzZ04 <havertzz04@gmail.com>
2023-03-14 13:04:57 -0500

    feat: :rocket: functions by declaration
    Probably the most popular way of these three, and the one we will be used to if we come from other programming languages, is the first, creating functions by declaration. This form allows you to declare a function that will exist throughout your code

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-// if the first operand is truthy,
-// AND returns the second operand:
-console.log( 1 && 0 ); // 0
-console.log( 1 && 5 ); // 5
-
-// if the first operand is falsy,
-// AND returns it. The second operand is ignored
-console.log( null && 5 ); // null
-console.log( 0 && "no matter what" ); // 0
+function greet(){
+    alert("Hello World");
+}
+greet();
